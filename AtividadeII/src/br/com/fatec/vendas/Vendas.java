cbf795d1d1cd41f1eea8459ac15f23791d0f2a26
Ana Beatriz Leite <silvaaraujoleite@gmail.com>
2017-09-01 18:56:51 -0300

    alteração no exercicio 5

--- AtividadeII/src/br/com/fatec/vendas/Vendas.java
+++ AtividadeII/src/br/com/fatec/vendas/Vendas.java
@@ -1,9 +1,19 @@
 package br.com.fatec.vendas;
+import java.util.Scanner;
 
 public class Vendas {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o número da conta do cliente ou -1 para sair: ");
+		int conta = sc.nextInt();
+		
+		while(conta != -1){
+			System.out.println("Digite o saldo inicial do mês: ");
+			int saldo 
+		}
 
 	}
 
